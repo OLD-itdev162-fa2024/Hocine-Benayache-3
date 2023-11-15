c724d3a148fa7d12c2f6127467bba9229f290cfe
Hocine Benayache <hocinebenayache@hotmail.com>
2023-11-15 00:45:59 -0600

    completed activity 9

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,23 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ViewPostComponent } from './view-post/view-post.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewPostComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +32,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
